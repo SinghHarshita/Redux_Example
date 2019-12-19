a4be870fdbd5aaf4363b2fc21022a0382de60183
Harshita <h2010singh@gmail.com>
2019-12-19 12:09:50 +0530

    Progress in posts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Posts from './components/Posts';
+import PostFrom from './components/PostForm';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <PostFrom />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
+    
   );
 }
 
